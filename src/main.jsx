fd1b895821cd68cb264d837cba740f0383bce8a0
Soham Agarwal <121bm0729@nitrkl.ac.in>
2024-01-06 09:42:55 +0530

    add package-lock.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import reactDOM from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+reactDOM.createRoot(document.getElementById('root')).render(<App/>)
